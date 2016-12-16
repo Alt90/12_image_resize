4c504bcebc632ed3953d750d91ddfeec28660d64
Alt13 <Alt90@rambler.ru>
2016-12-16 07:42:16 +0300

    Реализовал функцию работы с масштабом

--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from PIL import Image
 
 
 def get_args():
@@ -11,18 +14,44 @@ def get_args():
                         help='Width result imagefile')
     parser.add_argument('--height',
                         help='Height result imagefile')
-    parser.add_argument('--scale',
+    parser.add_argument('--scale', type=float,
                         help='Scale result imagefile')
     parser.add_argument('--output',
                         help='The directory where the imagefile to add')
     return parser.parse_args()
 
 
+def get_image_size(image):
+    return image.size
+
+
+def save_resized_image(output_file, output_path_to_file):
+    path, name = os.path.split(output_path_to_file)
+    size = list(get_image_size(output_file))
+    output_file.save('%s/%s__%sx%s%s' % (path,
+                                         name[:name.rindex('.')],
+                                         size[0],
+                                         size[1],
+                                         name[name.rindex('.'):]))
+
+
+def resize_image_by_scale(image, scale, output_path_to_file):
+    width, height = get_image_size(image)
+    output_file = image.resize((int(width*scale), int(height*scale)))
+    save_resized_image(output_file, output_path_to_file)
+
+
 def resize_image(args):
+    image = Image.open(args.path_to_file)
+    if (args.output is not None):
+        args.output = args.output + os.path.split(args.path_to_file)[1]
+    else:
+        args.output = args.path_to_file
     if (args.scale is not None):
         if (args.width or args.height is not None):
             print("Do not use scale options with width or height options!")
             exit()
+        resize_image_by_scale(image, args.scale, args.output)
 
 
 if __name__ == '__main__':
