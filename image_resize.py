6f800f13d67ae74c98c3cfee8ceb0089132a5f1d
Alt13 <Alt90@rambler.ru>
2016-12-16 06:32:59 +0300

    Добавлено описание работы скрипта и парсер параметров командной строки

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,30 @@
+import argparse
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="This script can change the size of image",
+    )
+    parser.add_argument('path_to_file',
+                        help='Path to imagefile')
+    parser.add_argument('--width',
+                        help='Width result imagefile')
+    parser.add_argument('--height',
+                        help='Height result imagefile')
+    parser.add_argument('--scale',
+                        help='Scale result imagefile')
+    parser.add_argument('--output',
+                        help='The directory where the imagefile to add')
+    return parser.parse_args()
+
+
+def resize_image(args):
+    if (args.scale is not None):
+        if (args.width or args.height is not None):
+            print("Do not use scale options with width or height options!")
+            exit()
 
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    resize_image(args)
